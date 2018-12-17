ceb182f71a8e85587a8fd5591b06d98950fe65b0
Vladan Velojic <v.velojic@vegaitsourcing.rs>
2018-12-17 19:21:36 +0100

    Cached users in localstorage

--- src/MyProvider.js
+++ src/MyProvider.js
@@ -1,17 +1,36 @@
 import React, { Component } from 'react';
 export const MyContext = React.createContext();
+
 export class MyProvider extends Component {
   state = {
-    users: []
+    users: [],
   }
-  componentDidMount() {
-    const url = 'https://reqres.in/api/users?per_page=10'
-    fetch(url)
-    .then(resp => resp.json())
-    .then(data => this.setState({
-      users: data.data
-    }))
+
+  async componentDidMount() {
+    const users = JSON.parse(localStorage.getItem('users')) || await this.fetchUsers();
+    this.setState({users})
   }
+
+  // componentDidMount() {
+  //   const users = JSON.parse(localStorage.getItem('users'))
+
+  //   if (users)
+  //     this.fetchUsers()
+  //       .then(data => users = data)
+
+  //   this.setState({users})
+  // }
+
+  async fetchUsers() {
+    const url = 'https://reqres.in/api/users?per_page=10';
+    const users = await fetch(url)
+      .then(data => data.json())
+      .then(data => data.data);
+
+    localStorage.setItem('users', JSON.stringify(users))
+    return users;
+  }
+
   render() {
     return (
       <MyContext.Provider value={this.state.users}>
@@ -20,3 +39,10 @@ export class MyProvider extends Component {
     )
   }
 }
+
+// const then = async () => {
+//   return new Promise(async (resolve, reject) => {
+//     const value = await fetch('');
+//     value ? resolve(value) : reject(value);
+//   });
+// }
